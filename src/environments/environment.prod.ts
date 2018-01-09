c599573d84c972d2d2dd8229650edb0c911568a8
Juan Aguirre <Jaguirr10@gmail.com>
2018-01-09 01:03:56 -0500

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  PIXABAY_API_URL: "https://pixabay.com/api/?key=",
+  PIXABAY_API_KEY: "7524976-acc80382522c41cee3d7641c8"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    PIXABAY_API_URL: "https://pixabay.com/api/?key=",
+    PIXABAY_API_KEY: "7524976-acc80382522c41cee3d7641c8"
+  };
+  
